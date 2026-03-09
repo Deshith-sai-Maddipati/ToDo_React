c4f4b24126fb4af719914a29d7ae4db8e648abfd
Deshith-sai-Maddipati <maddipati.deshith-sai@sixt.com>
2026-03-09 12:52:52 +0530

    Module FEderation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  federation({
+    name: 'todo',  
+    filename: 'remoteEntry.js',
+    exposes: {
+      './App': './src/App.jsx',  
+    },
+    shared: ['react', 'react-dom'],
+  }),],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+  server: {
+    port: 5001,
+    cors: true,
+  },
 })
